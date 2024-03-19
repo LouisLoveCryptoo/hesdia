41587740fa8cba374ffeb94302426ebe5c2d5e3b
LouisLoveCryptoo <louis.leschevin@gmail.com>
2024-03-19 14:42:41 +0100

    jeu de mémoire fini

--- pages/play/memory-game.vue
+++ pages/play/memory-game.vue
@@ -5,17 +5,35 @@
       v-for="(card, index) in playData"
       :key="index"
       @click="flipCard(index)"
-      :class="{ active: card.active }"
+      :class="{ active: !card.active }"
     >
-      <p :class="{ hidden: card.active }">
+      <p :class="{ hidden: !card.active }">
         {{ card.title }}
       </p>
     </div>
   </div>
-
+  <article v-if="state.descToDisplay">
+    <p>
+      {{ state.descToDisplay }}
+    </p>
+    <span @click="state.descToDisplay = ''">
+        X
+    </span>
+  </article>
   <button @click="reloadWindow()">Reset Game</button>
 </template>
 <script setup>
+/**
+ * Contient les données du jeu
+ * : (isFirstCard: boolean, oldIndex: number, descToDisplay: string)
+ * @type {reactive: state}
+ */
+const state = reactive({
+  isFirstCard: true,
+  oldIndex: null,
+  descToDisplay: "",
+});
+
 /**
  * Récupère les données du jeu de mémoire
  * et les transforme pour doubler le nombre de cartes
@@ -26,7 +44,7 @@ const { data: playData } = useFetch("/api/play/memory-game", {
   transform: (res) => {
     const concatArr = res.concat(res);
     const randomArr = concatArr.sort(() => (Math.random() > 0.5 ? 2 : -1));
-    return randomArr.map((item) => ({ ...item, active: false }));
+    return randomArr.map((item) => ({ ...item, active: false, match: false }));
   },
 });
 
@@ -38,11 +56,66 @@ const reloadWindow = () => {
   window.location.reload();
 };
 
+/**
+ * Retourne la carte retournée,
+ * si c'est la première carte, elle est retournée
+ * sinon, elle est comparée à la première carte
+ *
+ * @param {number} index
+ * @returns {void}
+ */
 const flipCard = (index) => {
-  playData.value[index].active = !playData.value[index].active;
-};
+  if (playData.value[index].active) return;
 
-console.log(playData.value);
+  /**
+   * Définit les cartes correspondantes comme étant match (donc active)
+   * en fonction de l'index précédent et de l'index actuel.
+   * @returns {void}
+   */
+  const setMatchingCard = () => {
+    playData.value.forEach((card) => {
+      if (card.id === state.oldIndex || card.id === playData.value[index].id) {
+        card.match = true;
+        card.active = true;
+        state.descToDisplay = card.desc;
+        console.log(state.descToDisplay);
+      }
+    });
+  };
+
+  /**
+   * Réinitialise les cartes retournées
+   * @returns {void}
+   */
+  const resetFlip = () => {
+    setTimeout(() => {
+      playData.value.forEach((card) => {
+        if (
+          card.id === state.oldIndex ||
+          card.id === playData.value[index].id
+        ) {
+          card.active = false;
+        }
+      });
+    }, 600);
+  };
+
+  if (state.isFirstCard) {
+    playData.value[index].active = true;
+    state.isFirstCard = false;
+    state.oldIndex = playData.value[index].id;
+  } else {
+    if (state.oldIndex === playData.value[index].id) {
+      playData.value[index].active = true;
+      setMatchingCard();
+      console.log("match");
+    } else {
+      playData.value[index].active = true;
+      resetFlip();
+    }
+    state.isFirstCard = true;
+  }
+};
 </script>
 <style>
 .hidden {
@@ -67,10 +140,11 @@ console.log(playData.value);
   color: white;
 }
 
-.memory__game div, .memory__game div p {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.memory__game div,
+.memory__game div p {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .memory__game div p {
