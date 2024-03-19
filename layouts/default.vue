b75fe652ed5155a41d81a508557b0174c3dfea97
LouisLoveCryptoo <louis.leschevin@gmail.com>
2024-03-19 14:53:07 +0100

    couleurs variables

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,4 +16,13 @@
 body {
   overflow-x: hidden;
 }
+
+:root {
+  --color-orange-dark:#FF8800;
+  --color-orange-light:#FFA750;
+  --color-text:#353838;
+
+  --background-color-main:#E5D1BD;
+  --background-color-secondary:#F9EEE8;
+}
 </style>
