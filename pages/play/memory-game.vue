899fc6ef35b1a3d664aa1f3ca5c28ee3f78f9b47
LouisLoveCryptoo <louis.leschevin@gmail.com>
2024-03-21 11:38:27 +0100

    oui

--- pages/play/memory-game.vue
+++ pages/play/memory-game.vue
@@ -1,26 +1,26 @@
 <template>
-  <h1>Memory Game</h1>
-  <div class="memory__game" v-if="playData">
-    <div
-      v-for="(card, index) in playData"
-      :key="index"
-      @click="flipCard(index)"
-      :class="{ active: !card.active }"
-    >
-      <p :class="{ hidden: !card.active }">
-        {{ card.title }}
-      </p>
+  <main>
+    <h1>Memory Game</h1>
+    <div class="memory__game" v-if="playData">
+      <div
+        v-for="(card, index) in playData"
+        :key="index"
+        @click="flipCard(index)"
+        :class="{ active: !card.active }"
+      >
+        <p :class="{ hidden: !card.active }">
+          {{ card.title }}
+        </p>
+      </div>
     </div>
-  </div>
-  <article v-if="state.descToDisplay">
-    <p>
-      {{ state.descToDisplay }}
-    </p>
-    <span @click="state.descToDisplay = ''">
-        X
-    </span>
-  </article>
-  <button @click="reloadWindow()">Reset Game</button>
+    <article v-if="state.descToDisplay">
+      <p>
+        {{ state.descToDisplay }}
+      </p>
+      <span @click="state.descToDisplay = ''"> X </span>
+    </article>
+    <button-phone style="white-space: nowrap;" class="button" :dark="true" @click="reloadWindow()">Reset Game</button-phone>
+  </main>
 </template>
 <script setup>
 /**
@@ -117,18 +117,38 @@ const flipCard = (index) => {
   }
 };
 </script>
-<style>
+<style scoped>
+main {
+  padding: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  height: 100%;
+}
+
 .hidden {
   visibility: hidden;
 }
+
+main h1 {
+  font-size: 2rem;
+  white-space: nowrap;
+}
+
 .memory__game {
-  width: 440px;
+  width: 430px;
   height: 430px;
   display: flex;
   flex-wrap: wrap;
   gap: 10px;
   transform-style: preserve-3d;
   perspective: 500px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 .memory__game div {
   position: relative;
@@ -137,7 +157,18 @@ const flipCard = (index) => {
   font-size: 1rem;
   text-align: center;
   transition: 0.25s;
-  color: white;
+  color: var(--color-orange-dark);
+}
+
+@media screen and (max-width: 800px) {
+  .memory__game {
+    width: 80vw;
+    height: min-content;
+  }
+  .memory__game div {
+    width: 90px;
+    height: 90px;
+  }
 }
 
 .memory__game div,
@@ -148,18 +179,24 @@ const flipCard = (index) => {
 }
 
 .memory__game div p {
-  color: black;
+  color: var(--color-orange-dark);
   z-index: 10;
   padding: 5px;
   background: whitesmoke;
   height: 100%;
+  font-size: 0.8rem;
+}
+
+main .button h4 {
+  font-size: 1.5rem;
+  white-space: nowrap;
 }
 
 .memory__game div::after {
   content: "";
   position: absolute;
   inset: 0;
-  background: #209d7b;
+  background: var(--bg-color-main);
   transition: 0.25s;
   transform: rotateY(0deg);
   width: 100%;
@@ -170,5 +207,22 @@ const flipCard = (index) => {
 
 .memory__game div.active {
   transform: rotateY(180deg);
+}
+
+article {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  background: var(--color-orange-light);
+  color: var(--bg-color-primary);
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+}
+article span {
+  cursor: pointer;
 }
 </style>
